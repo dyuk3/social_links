21ef7a9165ca91c7ab11abbffadef3c6ba76e109
unknown <vaibhavsinghyo@gmail.com>
2025-11-11 18:48:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import user from './avatar-jessica.jpeg';
 
 function App() {
+  const links = ['Github', 'Frontend Mentor', 'Linkedin', 'Twitter', 'Instagram'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='link_container'>
+        <div className='user_info'>
+          <img className='userImage' src={user} alt='userImg' />
+          <h2>Jessica Randall</h2>
+          <p className='country'>London,United Kingdom</p>
+          <p className='desc'>"Front-end developer and avid reader."</p>
+          <div className='links'>
+            {links.map((item) => (
+              <button className='link_btn'>{item}</button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
